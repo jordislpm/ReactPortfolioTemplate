ba40dbacb47feedb153565f37c2fabe65fc6aba8
jordislpm <jordislpm@gmail.com>
2023-05-06 12:03:32 -0400

    [feat] update footer

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -47,10 +47,10 @@ export default function BaseLayout() {
             <Grid item>
                <Box component={'footer'} display={'flex'} flexDirection={'column'} alignItems={'center'}
                     py={'1.5rem'} sx={{opacity: 0.7}} width={'100%'}>
-                  <p>template created with &hearts; by <span 
+                  <p>created with &hearts; by <span 
                   //href={'https://paytonpierce.dev'}
-                  >Payton Pierce</span></p>
-                  <p>&copy; 2023</p>
+                  >jordis.dev</span></p>
+                  <p>&copy; 2022</p>
                </Box>
             </Grid>
          </Grid>
